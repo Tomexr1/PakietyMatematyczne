655c67ad8ae4971f58911570cb8bda42556994d4
JoannaKusy <275999@student.pwr.edu.pl>
2023-04-29 00:32:41 +0200

    praca nad dopasowaniem danych

--- raintomorrow.jl
+++ raintomorrow.jl
@@ -1,24 +1,23 @@
-using DataFrames: DataFrame, missing, eltype
+using DataFrames
 using CSV
 using XGBoost
 using PyCall
 using ScikitLearn
 using ScikitLearn.CrossValidation: train_test_split
+using Impute: Impute
+using Random
 # using ScikitLearn.Preprocessing: Label
 # @sk_import preprocessing: LabelEncoder
-@sk_import preprocessing: (LabelBinarizer, StandardScaler)
+@sk_import preprocessing: (LabelBinarizer, StandardScaler, OneHotEncoder)
 # @sk_import feature_extraction: DictVectorizer
 # using ScikitLearn.feature_extraction: DictVectorizer
 # using ScikitLearn: fit!
 
 
-weather_data = DataFrame(CSV.File("weather.csv", normalizenames=true, delim=","))
-# describe(weather_data)
+weather_data = DataFrame(CSV.File("weather.csv", normalizenames=true, delim=",", missingstring="NA"))
 unique(weather_data[!, :])
-# println(names(weather_data))
-# first(weather_data)
 select!(weather_data, Not("RISK_MM"))
-# println(names(weather_data))
+Impute.srs!(weather_data; rng=MersenneTwister(1234))
 # print(describe(weather_data))
 # df_dict = Dict(pairs(eachcol(weather_data)))
 
@@ -31,39 +30,49 @@ select!(weather_data, Not("RISK_MM"))
 # categorical_mask = (df.dtypes == object)
 # categorical_columns = 
 # eltype.(eachcol(weather_data))
-mapper = DataFrameMapper([([:WindGustDir,:WindGustSpeed,:WindDir9am,:WindDir3pm,:RainToday,:RainTomorrow], LabelBinarizer()), ([:MinTemp,:MaxTemp,:Rainfall,:Evaporation,:Sunshine,:WindSpeed9am,:WindSpeed3pm,:Humidity9am,:Humidity3pm,:Pressure9am,:Pressure3pm,:Cloud9am,:Cloud3pm,:Temp9am,:Temp3pm], nothing)])
+# mapper = DataFrameMapper([([:WindGustDir,:WindGustSpeed,:WindDir9am,:WindDir3pm,:RainToday,:RainTomorrow], LabelBinarizer()), ([:MinTemp,:MaxTemp,:Rainfall,:Evaporation,:Sunshine,:WindSpeed9am,:WindSpeed3pm,:Humidity9am,:Humidity3pm,:Pressure9am,:Pressure3pm,:Cloud9am,:Cloud3pm,:Temp9am,:Temp3pm], nothing)])
 
-mapper2 = DataFrameMapper([(:MinTemp, nothing),
-(:MaxTemp, nothing),
-(:Rainfall, nothing),
-(:Evaporation, nothing),
-(:Sunshine, nothing),
-(:WindSpeed9am, nothing),
-(:WindSpeed3pm, nothing),
-(:Humidity9am, nothing),
-(:Humidity3pm, nothing),
-(:Pressure9am, nothing),
-(:Pressure3pm, nothing),
-(:Cloud9am, nothing),
-(:Cloud3pm, nothing),
-(:Temp9am, nothing),
-(:Temp3pm, nothing),
-(:WindGustDir, LabelBinarizer()),
-(:WindGustSpeed, nothing),
-(:WindDir9am, LabelBinarizer()),
-(:WindDir3pm, LabelBinarizer()),
-(:WindSpeed9am, nothing),
-(:WindSpeed3pm, nothing),
-(:Humidity9am, nothing),
-(:Humidity3pm, nothing),
-(:Pressure9am, nothing),
-(:Pressure3pm, nothing),
-(:Cloud9am, nothing),
-(:Cloud3pm, nothing),
-(:Temp9am, nothing),
-(:Temp3pm, nothing),
+mapper2 = DataFrameMapper([([:MinTemp], StandardScaler()),
+([:MaxTemp], StandardScaler()),
+([:Rainfall], StandardScaler()),
+([:Evaporation], StandardScaler()),
+([:Sunshine], StandardScaler()),
+([:WindSpeed9am], StandardScaler()),
+([:WindSpeed3pm], StandardScaler()),
+([:Humidity9am], StandardScaler()),
+([:Humidity3pm], StandardScaler()),
+([:Pressure9am], StandardScaler()),
+([:Pressure3pm], StandardScaler()),
+([:Cloud9am], StandardScaler()),
+([:Cloud3pm], StandardScaler()),
+([:Temp9am], StandardScaler()),
+([:Temp3pm], StandardScaler()),
+([:WindGustDir], OneHotEncoder(sparse=false)),
+([:WindGustSpeed], StandardScaler()),
+([:WindDir9am], OneHotEncoder(sparse=false)),
+([:WindDir3pm], OneHotEncoder(sparse=false)),
+([:WindSpeed9am], StandardScaler()),
+([:WindSpeed3pm], StandardScaler()),
+([:Humidity9am], StandardScaler()),
+([:Humidity3pm], StandardScaler()),
+([:Pressure9am], StandardScaler()),
+([:Pressure3pm], StandardScaler()),
+([:Cloud9am], StandardScaler()),
+([:Cloud3pm], StandardScaler()),
+([:Temp9am], StandardScaler()),
+([:Temp3pm], StandardScaler()),
 (:RainToday, LabelBinarizer()),
 (:RainTomorrow, LabelBinarizer())]
 )
 # fit_transform!(mapper, weather_data)
 fit_transform!(mapper2, copy(weather_data))
+
+function partitionTrainTest(data, at = 0.7)
+    n = nrow(data)
+    idx = shuffle(1:n)
+    train_idx = view(idx, 1:floor(Int, at*n))
+    test_idx = view(idx, (floor(Int, at*n)+1):n)
+    data[train_idx,:], data[test_idx,:]
+end
+
+train,test = partitionTrainTest(weather_data, 0.7)
